4480e45fd19cfbb124d52fcbf07a491ad8db7f5b
mjc136 <mjcullen2015@gmail.com>
2024-11-07 16:59:22 +0000

    updated to stop deadlock
    added conditional to prevent philosophers from all grabbing left fork causing deadlock

--- Lab/DiningPhilosophers/DiningPhilosopher.go
+++ Lab/DiningPhilosophers/DiningPhilosopher.go
@@ -2,59 +2,62 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"sync"
 	"time"
-	"math/rand/v2"
 )
 
-func think(index int){
+func think(index int) {
 	var X time.Duration
-	X=time.Duration(rand.IntN(5))
-	time.Sleep(X * time.Second)//wait random time amount
-	fmt.Println("Phil: ",index,"was thinking")
+	X = time.Duration(rand.IntN(5))
+	time.Sleep(X * time.Second) //wait random time amount
+	fmt.Println("Phil: ", index, "was thinking")
 }
 
-func eat(index int){
+func eat(index int) {
 	var X time.Duration
-	X=time.Duration(rand.IntN(5))
-	time.Sleep(X * time.Second)//wait random time amount
-	fmt.Println("Phil: ",index,"was eating")
+	X = time.Duration(rand.IntN(5))
+	time.Sleep(X * time.Second) //wait random time amount
+	fmt.Println("Phil: ", index, "was eating")
 }
 
-
-func getForks(index int, forks map[int]chan bool){
-	forks[index] <- true
-	forks[(index+1)%5] <-true
+func getForks(index int, forks map[int]chan bool) {
+	if index%2 == 0 {
+		forks[index] <- true
+		forks[(index+1)%5] <- true // %5 ensure last philosopher can still reach first fork
+	} else {
+		forks[(index+1)%5] <- true // %5 ensure last philosopher can still reach first fork
+		forks[index] <- true
+	}
 }
 
-func putForks(index int, forks map[int]chan bool){
+func putForks(index int, forks map[int]chan bool) {
 	<-forks[index]
 	<-forks[(index+1)%5]
 }
 
-func doPhilStuff(index int, wg *sync.WaitGroup, forks map[int] chan bool){
-	for {
-		think(index)
-		getForks(index,forks)
-		eat(index)
-		putForks(index,forks)
+func doPhilStuff(index int, wg *sync.WaitGroup, forks map[int]chan bool) {
+	for i := 0; i < 10; i++ {
+		think(index)           // wait
+		getForks(index, forks) // picks up forks
+		eat(index)             // eats
+		putForks(index, forks) // puts forks down
 	}
 	wg.Done()
 }
 
-
 func main() {
 	var wg sync.WaitGroup
-	philCount :=5
+	philCount := 5
 	wg.Add(philCount)
 
-	forks:= make(map[int]chan bool)
-	for k := range philCount{
-		forks[k]=make(chan bool, 1)
-	}//set up forks
+	forks := make(map[int]chan bool)
+	for k := range philCount {
+		forks[k] = make(chan bool, 1)
+	} //set up forks
 	for N := range philCount {
 		go doPhilStuff(N, &wg, forks)
-	}//start philosophers
+	} //start philosophers
 	wg.Wait() //wait here until everyone (10 go routines) is done
 
-}//main
+} //main
